396359f70946c029f4b6d0a3b8eaa92be70ffed1
Jason <jason.huang.2@stonybrook.edu>
2020-10-02 16:15:38 -0400

    Pushing changes

---
+++ app.js
@@ -0,0 +1,64 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+const mongoose = require("mongoose");
+
+
+const MONGO_URI = 27017;
+const PORT  = 42069;
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var testAPIRouter = require("./routes/testAPI");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/testAPI", testAPIRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the MongoDB database");
+  });
+
+//   app.listen({ port: PORT }, () => {
+//     console.log(`Server running at port: ${PORT}`);
+//   });
