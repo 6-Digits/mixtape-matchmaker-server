7336e009eb0577672153275b5ccd7d8b3084f12b
Darren Kong <darren.kong@stonybrook.edu>
2020-10-16 16:14:58 -0400

    Changed all vars to let

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+let cors = require("cors");
 const mongoose = require("mongoose");
 
 
 const MONGO_URI = 27017;
 const PORT  = 42069;
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI");
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
+let testAPIRouter = require("./routes/testAPI");
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
---
+++ routes/account.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let router = express.Router();
+
+
+router.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
