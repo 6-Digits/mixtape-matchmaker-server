b43846d726a1854e6bb9e7b4ad72be58ffbe8e06
Darren Kong <darren.kong@stonybrook.edu>
2020-11-08 17:32:14 -0500

    Finished route for returning all user mixtapes

--- routes/mixtapeManagement.js
+++ routes/mixtapeManagement.js
@@ -15,9 +15,9 @@ router.get('/id/:id', async (req, res) => {
 	await mixtapes.findById(req.params.id, function (err, user) {
 		if (err) {
 			return res.status(500).send("There was a problem adding the information to the database.");
-		}else if(!user){
+		} else if (!user) {
 			return res.status(404).send("No user found.");
-		}else{
+		} else {
 			return res.status(200).send(user);
 		}
 	});
@@ -25,24 +25,65 @@ router.get('/id/:id', async (req, res) => {
 
 // Gets a list of mixtapes from the database based on their owner
 router.get('/uid/:id', async (req, res) => {
-	await mixtapes.find({ owner :req.params.id }, function (err, user) {
-		if (err) {
-			return res.status(500).send("There was a problem adding the information to the database.");
-		}else if(!user){
-			return res.status(404).send("No user found.");
-		}else{
-			return res.status(200).send(user);
+	await mixtapes.find({ owner: req.params.id }).then(async (mixtapes) => {
+		//console.log(mixtapes);
+		if (!mixtapes) {
+			return res.status(404).send("No mixtapes found.");
 		}
+		let requests = mixtapes.map((mixtape) => {
+			return new Promise(async (resolve) => {
+				await songs.find({ _id: { $in: mixtape.songList } }).then(async (songs) => {
+					await comments.find({ _id: { $in: mixtape.comments } }).then((comment) => {
+						mixtape['songList'] = songs;
+						mixtape['comments'] = comment;
+						resolve(mixtape);
+					}).catch((error) => {
+						console.log(error)
+						resolve(res.status(500).send("There was an error finding the comments."))
+					});
+				}).catch((error) => {
+					console.log(error);
+					resolve(res.status(500).send("There is a problem with finding the songs."))
+				});
+				resolve(mixtapes);
+			});
+		});
+		Promise.all(requests).then((result) => {
+			//console.log(result);
+			return res.status(200).send(result);
+		})
+	}).catch((error) => {
+		console.log(error);
+		return res.status(500).send("There is a problem with finding the mixtape.");
 	});
-});
+})
+
+let helper = (mixtapeArray, returnJSON) => {
+	mixtapeArray.forEach(async (mixtape) => {
+		await songs.find({ _id: { $in: mixtape.songList } }).then(async (songs) => {
+			await comments.find({ _id: { $in: mixtape.comments } }).then((comment) => {
+				mixtape['songList'] = songs;
+				mixtape['comments'] = comment;
+				returnJSON.push(mixtape);
+			}).catch((error) => {
+				console.log(error)
+				return res.status(500).send("There was an error finding the comments.")
+			});
+		}).catch((error) => {
+			console.log(error);
+			return res.status(500).send("There is a problem with finding the songs.")
+		})
+	})
+	return returnJSON;
+}
 
 // Gets a list of mixtapes from the database based their view count
 router.get('/popular', async (req, res) => {
-	await mixtapes.find().sort({ views: -1}).limit(20).then((result) => {
-		if(!result){
+	await mixtapes.find().sort({ views: -1 }).limit(20).then((result) => {
+		if (!result) {
 			return res.status(404).send("No mixtapes found.");
 		}
-		return res.status(200).send(result);	
+		return res.status(200).send(result);
 	}).catch((error) => {
 		console.log(error);
 		return res.send(500).send("There is a problem with accessing the database");
@@ -51,8 +92,8 @@ router.get('/popular', async (req, res) => {
 
 // Gets a list of mixtapes from the database based their view count
 router.get('/likes', async (req, res) => {
-	await mixtapes.find().sort({ hearts: -1}).limit(20).then((result) => {
-		if(!result){
+	await mixtapes.find().sort({ hearts: -1 }).limit(20).then((result) => {
+		if (!result) {
 			return res.status(404).send("No mixtapes found.");
 		}
 		return res.status(200).send(result);
@@ -63,16 +104,25 @@ router.get('/likes', async (req, res) => {
 });
 
 // Gets songs in that mixtape
+// Depreciated by Jason
 router.get('/viewMixtape/id/:id', async (req, res) => {
-	await mixtapes.findById(req.params.id).then(async (result) => {
-		if(!result){
+	await mixtapes.findById(req.params.id).then(async (mixtape) => {
+		if (!mixtape) {
 			return res.status(404).send("No mixtapes found.");
 		}
-		await songs.find({ _id : {$in: result.songList}}).then((result) => {
-			return res.status(200).send(result);
+		let returnJSON = JSON.parse(JSON.stringify(mixtape))
+		await songs.find({ _id: { $in: mixtape.songList } }).then(async (songs) => {
+			await comments.find({ _id: { $in: mixtape.comments } }).then((comment) => {
+				returnJSON['songList'] = songs;
+				returnJSON['comments'] = comment;
+				res.status(200).send(returnJSON);
+			}).catch((error) => {
+				console.log(error)
+				res.status(500).send("There was an error finding the comments.")
+			});
 		}).catch((error) => {
 			console.log(error);
-			return res.status(500).send("There is a problem with finding the song.")
+			return res.status(500).send("There is a problem with finding the songs.")
 		})
 	}).catch((error) => {
 		console.log(error);
@@ -84,8 +134,8 @@ router.get('/viewMixtape/id/:id', async (req, res) => {
 router.post('/createMixtape/id/:id', verifyToken, async (req, res) => {
 	await mixtapes.create({
 		owner: req.params.id,
-    }).then(async (result) => {
-		if(!result){
+	}).then(async (result) => {
+		if (!result) {
 			return res.status(404).send("There is a problem with creating the mixtape.");
 		}
 		return res.status(200).send(result);
@@ -98,7 +148,7 @@ router.post('/createMixtape/id/:id', verifyToken, async (req, res) => {
 // Deletes a mixtape in the database
 router.post('/deleteMixtape/id/:id', verifyToken, async (req, res) => {
 	await mixtapes.remove({ owner: req.params.id }).then(async (result) => {
-		if(!result){
+		if (!result) {
 			return res.status(404).send("There is a problem with removing the mixtape.");
 		}
 		return res.status(200).send(result);
@@ -115,8 +165,8 @@ router.post('/createComment', verifyToken, async (req, res) => {
 		owner: req.body.id,
 		mixtape: req.body.mixtape,
 		text: req.body.text
-    }).then(async (result) => {
-		if(!result){
+	}).then(async (result) => {
+		if (!result) {
 			return res.status(404).send("There is a problem with creating the comment.");
 		}
 		return res.status(200).send(result);
