3543dddd1d423bb4fc6d1e854a46299c23f9e5b8
Darren Kong <darren.kong@stonybrook.edu>
2020-11-08 18:24:11 -0500

    Changed all vars to lets

---
+++ routes/search.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const accounts = require('../models/account');
+const verifyToken = require('../authentication/verifyToken');
+const bcrypt = require('bcryptjs');
+
+const MAX_PREDICTIONS_COUNT = 5,
+    dictionary = [{
+        word: 'abba',
+        weight: 100
+    }, {
+        word: 'abcd',
+        weight: 90
+    }, {
+        word: 'abfe',
+        weight: 80
+    }, {
+        word: 'abbb',
+        weight: 110
+    }, {
+        word: 'abcf',
+        weight: 70
+    }, {
+        word: 'abcg',
+        weight: 120
+    }, {
+        word: 'accg',
+        weight: 120
+    }, {
+        word: 'adcg',
+        weight: 120
+    }],
+    map = {},
+    searchInputElement,
+    predictionsElement;
+
+function attachListeners() {
+    searchInputElement = document.getElementById('search-input');
+    predictionsElement = document.getElementById('predictions');
+
+    searchInputElement.addEventListener('keyup', function (event) {
+        letvalue = event.target.value,
+            predictions = predict(value) || [];
+        predictionsElement.innerHTML = predictions.join('<br>');
+    });
+};
+
+build(()=> {
+    let item, pathArray;
+    for (let i = 0; i < dictionary.length; i++) {
+        item = dictionary[i];
+        pathArray = item.word.split('');
+        addItemToMap(map, pathArray, item.weight, i);
+    };
+});
+
+addItemToMap((mapNode, pathArray, weight, index)=> {
+    letletter;
+
+    if (pathArray && pathArray.length) {
+        letter = pathArray.shift();
+
+        if (mapNode[letter]) {
+            managePredictions(mapNode[letter].predictions, index);
+        } else {
+            mapNode[letter] = {
+                predictions: [index],
+                children: {}
+            }
+        }
+        addItemToMap(mapNode[letter].children, pathArray, weight, index);
+    }
+})
+
+managePredictions((predictions, index)=> {
+    letinsertionIndex = getInsertionIndex(predictions, index);
+
+    predictions.splice(insertionIndex, 0, index);
+    if (predictions.length > MAX_PREDICTIONS_COUNT) {
+        predictions = predictions.splice(-1, 1);
+    }
+})
+
+function getInsertionIndex(array, index) {
+    letlow = 0,
+        high = array.length,
+        middle;
+
+    while (low < high) {
+        middle = low + high >>> 1;
+        if (dictionary[array[middle]].weight > dictionary[index].weight) {
+            low = middle + 1;
+        } else {
+            high = middle;
+        }
+    }
+    return low;
+};
+
+function predict(searchTerm) {
+    letpredictionObject = findPredictions(searchTerm.split(''), map),
+        result = [];
+
+    if (predictionObject) {
+        for (leti = 0; i < predictionObject.predictions.length; i++) {
+            result.push(dictionary[predictionObject.predictions[i]].word);
+        };
+        return result;
+    }
+};
+
+function findPredictions(searchTermArray, object) {
+    letpathArray,
+        key;
+
+    if (searchTermArray && searchTermArray.length) {
+        key = searchTermArray.shift();
+        if (object[key] && object[key].children && searchTermArray && searchTermArray.length) {
+            return findPredictions(searchTermArray, object[key].children);
+        } else {
+            return object[key];
+        }
+    }
+};
+
+buildMap();
+attachListeners();
