56c6b2955df0e3371816f3b4690ce39c37caad19
Darren Kong <darren.kong@stonybrook.edu>
2020-11-11 12:21:42 -0500

    changes to settings edits

--- routes/accountManagement.js
+++ routes/accountManagement.js
@@ -22,16 +22,30 @@ router.get('/id/:id', async (req, res) => {
 
 // Updates a single user in the database
 router.post('/id/:id', verifyToken, async (req, res) => {
-	//let hashedPassword = bcrypt.hashSync(req.body.password, 8);
-	await accounts.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
-		if (err) {
-			return res.status(500).send("There was a problem adding the information to the database.");
-		}else if(!user){
-			return res.status(404).send("No user found.");
-		}else{
-			res.status(200).send(user);
-		}
-	});
+	if (req.body.password == null || req.body.password == ""){
+		delete req.body['password']
+		await accounts.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+			if (err) {
+				return res.status(500).send("There was a problem adding the information to the database.");
+			}else if(!user){
+				return res.status(404).send("No user found.");
+			}else{
+				res.status(200).send(user);
+			}
+		});
+	}else{
+		let hashedPassword = bcrypt.hashSync(req.body.password, 8);
+		req.body['password'] = hashedPassword;
+		await accounts.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+			if (err) {
+				return res.status(500).send("There was a problem adding the information to the database.");
+			}else if(!user){
+				return res.status(404).send("No user found.");
+			}else{
+				res.status(200).send(user);
+			}
+		});
+	}
 })
 
 
