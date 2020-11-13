ecc746292cffb130314dbf789ec8b6983f26ef99
Darren Kong <darren.kong@stonybrook.edu>
2020-11-12 20:55:28 -0500

    Password fixed

--- routes/accountManagement.js
+++ routes/accountManagement.js
@@ -12,9 +12,9 @@ router.get('/id/:id', async (req, res) => {
 	await accounts.findById(req.params.id, function (err, user) {
 		if (err) {
 			return res.status(500).send("There was a problem adding the information to the database.");
-		}else if(!user){
+		} else if (!user) {
 			return res.status(404).send("No user found.");
-		}else{
+		} else {
 			res.status(200).send(user);
 		}
 	});
@@ -22,45 +22,32 @@ router.get('/id/:id', async (req, res) => {
 
 // Updates a single user in the database
 router.post('/id/:id', verifyToken, async (req, res) => {
-	if (req.body.password == null || req.body.password == ""){
-		let hashedPassword = bcrypt.hashSync(req.body.oldPassword, 8);
-		delete req.body['oldPassword']
-		req.body['password'] = hashedPassword;
-		await accounts.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
-			if (err) {
-				return res.status(500).send("There was a problem adding the information to the database.");
-			}else if(!user){
-				return res.status(404).send("No user found.");
-			}else{
-				res.status(200).send(user);
-			}
-		});
-	}else{
-		let hashedPassword = bcrypt.hashSync(req.body.oldPassword, 8);
-		delete req.body['oldPassword'];
-		req.body['password'] = hashedPassword;
-		await accounts.findById(req.params.id, {new: true}, async (err, user) => {
-			if (err) {
-				return res.status(500).send("There was a problem adding the information to the database.");
-			}else if(!user){
-				return res.status(404).send("No user found.");
-			}else{
-				if (hashedPassword == user.password){
-					await accounts.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
-						if (err) {
-							return res.status(500).send("There was a problem adding the information to the database.");
-						}else if(!user){
-							return res.status(404).send("No user found.");
-						}else{
-							res.status(200).send(user);
-						}
-					});
-				}else {
-					res.status(400).send("Wrong password!");
+	let hashedPassword = bcrypt.hashSync(req.body.oldPassword, 8);
+	delete req.body['oldPassword'];
+	await accounts.findById(req.params.id, { new: true }, async (err, user) => {
+		if (err) {
+			return res.status(500).send("There was a problem adding the information to the database.");
+		} else if (!user) {
+			return res.status(404).send("No user found.");
+		} else {
+			if (hashedPassword == user.password) {
+				if (req.body.password == null){
+					req.body['password'] = hashedPassword;
 				}
+				await accounts.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, user) {
+					if (err) {
+						return res.status(500).send("There was a problem adding the information to the database.");
+					} else if (!user) {
+						return res.status(404).send("No user found.");
+					} else {
+						res.status(200).send(user);
+					}
+				});
+			} else {
+				res.status(400).send("Wrong password!");
 			}
-		});
-	}
+		}
+	});
 })
 
 
