b4db02ca7ef19d09ffc42da4425f5c29b9b0d149
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-11-18 03:12:21 -0500

    implemented youtube search api

--- routes/youtube.js
+++ routes/youtube.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const http = require('http');
-const https = require('https');
+const axios = require('axios');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,7 +19,7 @@ function convertTime(duration) {
 		array = [array[0], 0, 0];
 	}
 	
-	seconds = 0;
+	let seconds = 0;
 	if (array.length === 3) {
 		seconds += parseInt(array[0]) * 3600;
 		seconds += parseInt(array[1]) * 60;
@@ -33,45 +32,87 @@ function convertTime(duration) {
 	if (array.length === 1) {
 		seconds += parseInt(array[0]);
 	}
-	return seconds
+	return seconds;
 }
 
-
+// uses 1 youtube api quota point per call
 router.get('/video/:id', async (req, res) => {
 	const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=${req.params.id}&key=${process.env.YOUTUBE_API_KEY}`;
 	
-	https.get(url, (response) => {
-		let body = "";
-		
-		response.on('data', (data) => {
-			body += data;
-		});
-		response.on('end', () => {
-			try {
-				let json = JSON.parse(body);
-				let songData = json['items'][0];
+	axios.get(url)
+		.then((response) => {
+			if (response.status == 200) {
+				let data = response.data['items'][0];
 				
 				let song = {
-					id: songData['id'],
-					title: songData['snippet']['title'],
-					author: songData['snippet']['channelTitle'],
-					thumbnail: songData['snippet']['thumbnails']['maxres']['url'],
-					language: songData['snippet']['defaultLanguage'] ? songData['snippet']['defaultLanguage'] : 'en',
-					genres: songData['snippet']['tags'],
-					duration: convertTime(songData['contentDetails']['duration'])
+					id: data['id'],
+					title: data['snippet']['title'],
+					url: `https://www.youtube.com/watch?v=${req.params.id}`,
+					author: data['snippet']['channelTitle'],
+					thumbnail: data['snippet']['thumbnails']['standard']['url'],
+					language: data['snippet']['defaultLanguage'] ? data['snippet']['defaultLanguage'] : 'en',
+					genres: data['snippet']['tags'],
+					duration: convertTime(data['contentDetails']['duration'])
 				};
 				
 				return res.status(200).send(song);
 			}
-			catch (error) {
-				console.log(error.message);
-				return res.status(500).send(error.message);
+			else {
+				res.status(404).send("Invalid YouTube ID");
 			}
+		})
+		.catch((error) => {
+			console.log(error.message);
+			return res.status(500).send(error.message);
 		});
-	}).on('error', (err) => {
-		console.log(err);
-		return res.status(500).send(err.message);
-	});
+})
+
+// does not use any youtube api quota points
+router.get('/search/:query', async (req, res) => {
+	const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(req.params.query)}`;
+	
+	axios.get(url)
+		.then((response) => {
+			if (response.status == 200) {
+				try {
+					let match = response.data.match(/ytInitialData[^{]*(.*);\s*\/\/ scraper_data_end/s);
+					let data = JSON.parse(match[1]);
+					let results = data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents;
+					
+					let json = { results: [] }
+					results.filter(x => x.hasOwnProperty("itemSectionRenderer")).forEach(result => {
+						result.itemSectionRenderer.contents.forEach(content => {
+							if (content.hasOwnProperty("videoRenderer")) {
+								let render = content.videoRenderer;
+								let song = {
+									id: render.videoId,
+									title: render.title.runs.reduce((a, b) => a + b.text, ""),
+									url: `https://www.youtube.com${render.navigationEndpoint.commandMetadata.webCommandMetadata.url}`,
+									author: render.ownerText.runs[0].text,
+									duration: render.lengthText ? render.lengthText.simpleText : "Live",
+									thumbnail: render.thumbnail.thumbnails[render.thumbnail.thumbnails.length - 1].url,
+								};
+								json['results'].push(song);
+							}
+						});
+					});
+					json['total'] = json['results'].length;
+					
+					return res.status(200).send(json);
+				}
+				catch (error) {
+					console.log(error.message);
+					res.status(500).send(error.message);
+				}
+			}
+			else {
+				res.status(404).send("Invalid query");
+			}
+		})
+		.catch((error) => {
+			console.log(error.message);
+			return res.status(500).send(error.message);
+		});
 })
 
 module.exports = router;
