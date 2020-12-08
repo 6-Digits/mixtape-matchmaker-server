af1f0d659d75c253485ae5b4bb1f3b2d084f011f
Jason <jason.huang.2@stonybrook.edu>
2020-12-08 14:55:16 -0500

    exhibiting issues with post

--- routes/profileManagement.js
+++ routes/profileManagement.js
@@ -126,6 +126,7 @@ router.get('/notifications/uid/:uid', async (req, res) => {
 // Note that calling this route sends back the delete operation
 // http://localhost:42069/api/profile/notificatons/nid/:nid
 router.post('/notifications/nid/:nid', async (req, res) => {
+	console.log("starting delete of notification");
 	await notifications.deleteOne({ _id: req.params.id }).then((result) => {
 		if (!result) {
 			return res.status(404).send("No user found.");
