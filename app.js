fd220a932071eccbfcc6de618c49f53319fc63f2
Jason <jason.huang.2@stonybrook.edu>
2020-12-20 11:46:41 -0500

    disable of the server

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+// require('dotenv').config()
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
 
-const app = express()
-	.use(cors())
-	.use(express.static(__dirname))
-	.use(express.json())
-	.use(express.urlencoded({ extended: true }));
+// const app = express()
+// 	.use(cors())
+// 	.use(express.static(__dirname))
+// 	.use(express.json())
+// 	.use(express.urlencoded({ extended: true }));
 
-const server = require('http').createServer(app);
-const io = require("socket.io").listen(server);
+// const server = require('http').createServer(app);
+// const io = require("socket.io").listen(server);
 
-const createSockets = require('./scripts/createSockets');
-const createChats = require('./scripts/createChats');
-const createMatches = require('./scripts/createMatches');
+// const createSockets = require('./scripts/createSockets');
+// const createChats = require('./scripts/createChats');
+// const createMatches = require('./scripts/createMatches');
 
-createSockets(io);
+// createSockets(io);
 
-// background scripts
-const CHAT_DELAY = 2 * 60000;
-setTimeout(async function timer() {
-	await createChats(io);
-	console.log("Chats created")
-	setTimeout(timer, CHAT_DELAY);
-}, CHAT_DELAY);
+// // background scripts
+// const CHAT_DELAY = 2 * 60000;
+// setTimeout(async function timer() {
+// 	await createChats(io);
+// 	console.log("Chats created")
+// 	setTimeout(timer, CHAT_DELAY);
+// }, CHAT_DELAY);
 
-const MATCH_DELAY = 5 * 60000;
-setTimeout(async function timer() {
-	await createMatches(io);
-	console.log("Matches created")
-	setTimeout(timer, MATCH_DELAY);
-}, MATCH_DELAY);
+// const MATCH_DELAY = 5 * 60000;
+// setTimeout(async function timer() {
+// 	await createMatches(io);
+// 	console.log("Matches created")
+// 	setTimeout(timer, MATCH_DELAY);
+// }, MATCH_DELAY);
 
-// routes
-app.use("/", require("./routes/index"));
-app.use('/account', require('./routes/accountManagement'));
-app.use('/profile', require('./routes/profileManagement'));
-app.use('/mixtape', require('./routes/mixtapeManagement'));
-app.use('/match', require('./routes/matchManagement'));
-app.use('/search', require('./routes/search'));
-app.use('/youtube', require('./routes/youtube'));
+// // routes
+// app.use("/", require("./routes/index"));
+// app.use('/account', require('./routes/accountManagement'));
+// app.use('/profile', require('./routes/profileManagement'));
+// app.use('/mixtape', require('./routes/mixtapeManagement'));
+// app.use('/match', require('./routes/matchManagement'));
+// app.use('/search', require('./routes/search'));
+// app.use('/youtube', require('./routes/youtube'));
 
-// initialize MongoDB
-const mongoUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vq24q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-mongoose.connect(mongoUri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-}).then(() => {
-	console.log("Connected to the MongoDB database");
-});
+// // initialize MongoDB
+// const mongoUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vq24q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+// mongoose.connect(mongoUri, {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// }).then(() => {
+// 	console.log("Connected to the MongoDB database");
+// });
 
-server.listen(process.env.PORT, () => {
-	console.log(`Server started on port ${process.env.PORT}`);
-});
+// server.listen(process.env.PORT, () => {
+// 	console.log(`Server started on port ${process.env.PORT}`);
+// });
 
-module.exports = app;
+// module.exports = app;
